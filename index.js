9a2e95d50fcae94cd60b885916e6c1d4ca0dbe8f
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-09-10 14:59:06 +0530

    completed exercises mentioned in Lesson-4

---
+++ index.js
@@ -0,0 +1,41 @@
+// Exercise - 1
+// const userName = prompt("What is your name?");
+
+// alert(`Welcome to Minion translater ${userName}`);
+
+// Exercise - 2
+// const btn = document.querySelector("button");
+
+// console.log(btn);
+
+// btn.addEventListener("click", () => {
+//   console.log("Button Clicked");
+// });
+
+// Exercise - 3
+// const textBox = document.querySelector("textarea");
+
+// console.log(textBox);
+
+// textBox.addEventListener("input", (e) => {
+//   console.log(e.target.value);
+// });
+
+// const inputEl = document.querySelector('input[name="translator"]');
+// console.log(inputEl);
+
+// const btn = document.querySelector("button");
+// const textEl = document.querySelector("textarea");
+
+// btn.addEventListener("click", () => {
+//   console.log(textEl.value);
+// });
+
+// Exercise - 5
+const btn = document.querySelector("button");
+const outputDiv1 = document.querySelector("div");
+
+btn.addEventListener("click", () => {
+  outputDiv1.innerText =
+    "Button click event resulted in setting this inner text";
+});
