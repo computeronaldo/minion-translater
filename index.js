52ec7f75e7be4db71204a222b6d0ac330c05538d
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-09-13 15:23:28 +0530

    feat: added translation functionality

--- index.js
+++ index.js
@@ -39,3 +39,49 @@
 //   outputDiv1.innerText =
 //     "Button click event resulted in setting this inner text";
 // });
+
+const inputText = document.querySelector(".input-text");
+const submitBtn = document.querySelector(".translate");
+const outputCont = document.querySelector(".output");
+
+const url = "https://google-translate1.p.rapidapi.com/language/translate/v2";
+
+const submitHandler = async () => {
+  const encodedText = encodeURI(inputText.value);
+
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "application/gzip",
+        "X-RapidAPI-Key": "ae4018746fmshdba8b95361b1d17p16756bjsn2f05ba5ea3b0",
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+      },
+      body: new URLSearchParams({
+        q: encodedText,
+        target: "es",
+        source: "en",
+      }),
+    };
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      alert("Something went wrong");
+      inputText.value = "";
+      throw new Error("Something went wrong");
+    }
+
+    const result = await response.text();
+    const translatedText = decodeURIComponent(
+      JSON.parse(result).data.translations[0].translatedText
+    );
+
+    outputCont.innerHTML = translatedText;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+submitBtn.addEventListener("click", submitHandler);
